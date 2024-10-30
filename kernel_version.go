7defa6cdcaebcb9b4fcd495a54833b5ef81fe538
Leon Hwang <hffilwlqm@gmail.com>
2024-10-30 21:47:04 +0800

    kernel version: Extract version from complicated uname -r
    semver library is unable to parse kernel version like `5.10.134-16.3.al8.x86_64`.
    
    Then, it is enough to extract the version `5.10.134`.
    
    Signed-off-by: Leon Hwang <hffilwlqm@gmail.com>

--- kernel_version.go
+++ kernel_version.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/blang/semver/v4"
 	"github.com/shirou/gopsutil/v3/host"
@@ -13,7 +14,7 @@ func isKernelVersionGte_5_16() (bool, error) {
 		return false, fmt.Errorf("failed to get kernel version: %v", err)
 	}
 
-	version, err := semver.Make(release)
+	version, err := semver.Make(extractVersion(release))
 	if err != nil {
 		return false, fmt.Errorf("failed to parse kernel version: %v", err)
 	}
@@ -21,4 +22,24 @@ func isKernelVersionGte_5_16() (bool, error) {
 	version_5_16 := semver.MustParse("5.16.0")
 
 	return version.GTE(version_5_16), nil
+}
+
+func extractVersion(release string) string {
+	parts := strings.SplitN(release, ".", 3)
+	if len(parts) < 3 {
+		return release
+	}
+
+	patch := parts[2]
+
+	// Find the first non-digit character
+	for i, c := range patch {
+		if c < '0' || c > '9' {
+			patch = patch[:i]
+			break
+		}
+	}
+
+	parts[2] = patch
+	return strings.Join(parts, ".")
 }
